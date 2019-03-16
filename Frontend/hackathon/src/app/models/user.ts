01a1dcf3d85a55af8cd94e9c14bee4597e8b4b4d
Stefan S <stefanlawr@gmail.com>
2019-03-16 17:29:39 -0400

    Created getUser service

---
+++ Frontend/hackathon/src/app/models/user.ts
@@ -0,0 +1,17 @@
+import { HttpHeaders } from '@angular/common/http';
+
+export const baseUrl = 'http://localhost:8080/';
+
+export const httpOption = {
+  headers: new HttpHeaders({
+    'Content-Type': 'applications/json'
+  })
+};
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password?: string;
+  schoolId: string;
+}
