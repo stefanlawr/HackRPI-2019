885ebe516786e3d2238e612447a91916513a6627
Stefan S <stefanlawr@gmail.com>
2019-03-17 02:53:17 -0400

    Updated internship model

--- Frontend/hackathon/src/app/campus-hub/internships/internship.model.ts
+++ Frontend/hackathon/src/app/campus-hub/internships/internship.model.ts
@@ -1,5 +1,5 @@
 export interface Internship {
-  e_id: string;         // event id
+  e_id?: string;        // event id
   e_t_id: string;       // event type id
   s_id: string;         // school id
   name: string;         // event name
