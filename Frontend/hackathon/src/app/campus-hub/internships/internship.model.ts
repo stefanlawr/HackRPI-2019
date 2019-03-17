f74000d5298aa3e07f7c22faf05f3d6a0bbc6040
Stefan S <stefanlawr@gmail.com>
2019-03-17 02:50:56 -0400

    added description to internship model

--- Frontend/hackathon/src/app/campus-hub/internships/internship.model.ts
+++ Frontend/hackathon/src/app/campus-hub/internships/internship.model.ts
@@ -1,7 +1,8 @@
 export interface Internship {
-  e_id: string;      // event id
-  e_t_id: string;    // event type id
-  s_id: string;      // school id
-  name: string;      // event name
-  location: string;  // location
+  e_id: string;         // event id
+  e_t_id: string;       // event type id
+  s_id: string;         // school id
+  name: string;         // event name
+  location: string;     // location
+  description: string;  // description
 }
