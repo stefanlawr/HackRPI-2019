8b65d3273b5870d1d8f3bb2e6afe45e2592fe677
Stefan S <stefanlawr@gmail.com>
2019-03-17 00:39:07 -0400

    typo corrected in header

--- Frontend/hackathon/src/app/services/data.service.ts
+++ Frontend/hackathon/src/app/services/data.service.ts
@@ -1,30 +1,20 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-import {handleError, User} from '../models/user';
+import { handleError, User } from '../models/user';
 
-import {baseUrl, httpOption} from './../models/user';
+import { baseUrl, httpOption } from './../models/user';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataService {
   constructor(private http: HttpClient) {}
 
   // TODO: Pass userID to get_user instead of hardcoded number
   getUser(): Observable<User> {
-    // if (!sessionStorage.getItem('user')) {
-    //   this.http.get<User>(`${baseUrl}/get_user/5`, httpOption)
-    //       .subscribe(res => {
-    //         sessionStorage.setItem('user', JSON.stringify(res));
-    //       });
-
-    //   return this.http.get<User>(`${baseUrl}/get_user/5`, httpOption)
-    //       .pipe(catchError(handleError('DATA: getUser()', {} as User)));
-    // }
-
-    // return of(JSON.parse(sessionStorage.getItem('user')) as User);
-    return this.http.get<User>(`${baseUrl}/get_user/5`, httpOption)
-        .pipe(catchError(handleError('DATA: getUser()', {} as User)));
+    return this.http
+      .get<User>(`${baseUrl}/get_user/5`, httpOption)
+      .pipe(catchError(handleError('DATA: getUser()', {} as User)));
   }
 }
